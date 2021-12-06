0170844659cdf631fb9a5dd3cc09833b839c50ab
kayque <kayquegps2@gmail.com>
2021-12-06 11:06:09 -0300

    Classe categoria finalizada

---
+++ pi_2/src/main/java/br/org/generation/pi/model/Categoria.java
@@ -0,0 +1,55 @@
+package br.org.generation.pi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O atributo nome é obrigatorio!")
+	@Size(max = 255)
+	private String nome;
+
+	@NotNull(message = "O atributo palavra_chave é obrigatorio!")
+	@Size(max = 255)
+	private String palavraChave;
+
+	@NotNull(message = "O atributo descricao é obrigatorio")
+	@Size(max = 1000)
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
