d30c7832e2f0095e2bbe72dc2910c342206b5a77
devlarissa <devlarissasilva@gmail.com>
2021-12-10 11:10:32 -0300

    Adicionando classe usuario

--- pi_2/src/main/java/br/org/generation/dandara/model/Produto.java
+++ pi_2/src/main/java/br/org/generation/dandara/model/Produto.java
@@ -44,6 +44,9 @@ public class Produto {
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
 
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
